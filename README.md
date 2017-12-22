f8702779e4f5da5e0d2796bb8789dad389358d1d
denox-PC\denox <danieloliveira@denox.com.br>
2017-12-22 11:46:24 -0200

    Added onFinishedTyping listener

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Add to your Gradle file:
 
 Then add this to your "depedencies":
    
-    implementation 'com.github.shinayser:ShinayserEssentials:1.5.0'
+    implementation 'com.github.shinayser:ShinayserEssentials:1.5.1'
 
 # Json creation DSL
 
---
+++ library/src/main/java/com/shinayser/essentials/util/FinishTypingTextWatcher.kt
@@ -0,0 +1,37 @@
+package com.shinayser.essentials.util
+
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import com.mcxiaoke.koi.ext.cancelAll
+
+/**
+ * Created by denox on 18/12/2017.
+ */
+typealias FinishedTypingListener = (String) -> Unit
+
+class FinishTypingTextWatcher(private val delay: Long, val listener: FinishedTypingListener) : TextWatcher {
+
+    var handler = Handler()
+
+    override fun afterTextChanged(s: Editable?) {
+
+        handler.postDelayed({
+            listener.invoke(s.toString())
+        }, delay)
+
+
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        handler.cancelAll()
+    }
+
+}
+
+fun EditText.onFinishedTyping(delay: Long = 600, listener: FinishedTypingListener) {
+    this.addTextChangedListener(FinishTypingTextWatcher(delay, listener))
+}
