7987a220091d572378e2e78bd583974362696a07
denox-PC\denox <danieloliveira@denox.com.br>
2018-01-10 16:22:17 -0200

    Added time interpolator into "porpertyAnimation" method

--- library/src/main/java/com/shinayser/essentials/util/View.kt
+++ library/src/main/java/com/shinayser/essentials/util/View.kt
@@ -2,6 +2,7 @@ package com.shinayser.essentials.util
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.animation.TimeInterpolator
 import android.support.v7.widget.SwitchCompat
 import android.support.v7.widget.Toolbar
 import android.view.View
@@ -10,9 +11,17 @@ import android.widget.*
 /**
  * Created by denox on 10/11/2017.
  */
-fun View.visible() { visibility = View.VISIBLE }
-fun View.gone() { visibility = View.GONE }
-fun View.invisible() { visibility = View.INVISIBLE }
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
 
 fun View.findTextView(resId: Int): TextView = findViewById(resId)
 fun View.findEditText(resId: Int): EditText = findViewById(resId)
@@ -24,16 +33,18 @@ fun View.findButton(resId: Int): Button = findViewById(resId)
 fun View.findImageButton(resId: Int): ImageButton = findViewById(resId)
 fun View.findToolbar(resId: Int): Toolbar = findViewById(resId)
 
-fun View.propertyAnimation(property: String, duration: Long, fromValue: Float, toValue: Float, listener: Animator.AnimatorListener? = null) {
+fun View.propertyAnimation(property: String, duration: Long, fromValue: Float, toValue: Float, interpolator: TimeInterpolator? = null, listener: Animator.AnimatorListener? = null) {
     val objectAnimation = ObjectAnimator.ofFloat(this, property, fromValue, toValue)
     objectAnimation.duration = duration
+    objectAnimation.interpolator = interpolator
     listener?.let { objectAnimation.addListener(listener) }
     objectAnimation.start()
 }
 
-fun View.propertyAnimation(property: String, duration: Long, fromValue: Int, toValue: Int, listener: Animator.AnimatorListener? = null) {
+fun View.propertyAnimation(property: String, duration: Long, fromValue: Int, toValue: Int, interpolator: TimeInterpolator? = null, listener: Animator.AnimatorListener? = null) {
     val objectAnimation = ObjectAnimator.ofInt(this, property, fromValue, toValue)
     objectAnimation.duration = duration
+    objectAnimation.interpolator = interpolator
     listener?.let { objectAnimation.addListener(listener) }
     objectAnimation.start()
 }
