54be7d6b95bd4c125ae8656375c17780b509c367
denox-PC\denox <danieloliveira@denox.com.br>
2018-01-26 18:40:17 -0200

    Added missing "onKitkat" method

--- library/src/main/java/com/shinayser/essentials/simplifiedconditions/ApiLevel.kt
+++ library/src/main/java/com/shinayser/essentials/simplifiedconditions/ApiLevel.kt
@@ -20,6 +20,7 @@ inline fun onAPILevel(apiLevel: Int, lambda: () -> Unit): ExecutedCode {
 inline fun onJellyBean(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.JELLY_BEAN, lambda)
 inline fun onJellyBeanMR1(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.JELLY_BEAN_MR1, lambda)
 inline fun onJellyBeanMR2(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.JELLY_BEAN_MR2, lambda)
+inline fun onKitkat(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.KITKAT, lambda)
 inline fun onLollipop(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.LOLLIPOP, lambda)
 inline fun onLollipopMR1(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.LOLLIPOP_MR1, lambda)
 inline fun onMarshmallow(lambda: () -> Unit) = onAPILevel(Build.VERSION_CODES.M, lambda)
