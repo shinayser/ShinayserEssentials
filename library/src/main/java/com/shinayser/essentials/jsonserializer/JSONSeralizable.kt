d5ca91b06827950dd4b8ef5916c68a4bd28d86f1
danieloliveira <danieloliveira@denox.com.br>
2018-03-07 14:41:40 -0300

    - Added JSONSerializable interfaces

---
+++ library/src/main/java/com/shinayser/essentials/jsonserializer/JSONSeralizable.kt
@@ -0,0 +1,118 @@
+package com.shinayser.essentials.jsonserializer
+
+import com.shinayser.essentials.util.jsonOf
+import org.json.JSONObject
+
+/**
+ * Created by denox on 15/12/2017.
+ */
+interface JSONSerializable {
+    fun serialize(): JSONObject
+    fun deserialize(json: JSONObject)
+}
+
+/*
+   It can only serialize objects that implement JSONSerializable or primitive types.
+ */
+interface SimpleJSONSerializable : JSONSerializable {
+
+    override fun serialize(): JSONObject {
+
+        return jsonOf {
+
+            this@SimpleJSONSerializable::class.java.declaredFields.forEach { field ->
+                field.isAccessible = true
+
+                try {
+                    when (field.type) {
+
+                        Boolean::class.javaObjectType,
+                        Long::class.javaObjectType,
+                        Int::class.javaObjectType,
+                        Float::class.javaObjectType,
+                        Double::class.javaObjectType,
+                        String::class.javaObjectType -> field.name otopt field.get(this@SimpleJSONSerializable)
+
+
+                        else -> {
+
+                            if (JSONSerializable::class.java.isAssignableFrom(field.type)) {
+                                field.name otopt (field.get(this@SimpleJSONSerializable) as JSONSerializable).serialize()
+                            }
+
+                        }
+
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+
+            }
+
+        }
+
+    }
+
+    override fun deserialize(json: JSONObject) {
+
+        json.keys().forEach { key ->
+            val field = this::class.java.declaredFields.firstOrNull { it.name == key }
+
+            field?.let {
+
+                field.isAccessible = true
+
+                try {
+
+                    when {
+
+                        field.type == Long::class.javaObjectType -> field.set(this, json.getLong(key))
+                        field.type == Int::class.javaObjectType -> field.set(this, json.getInt(key))
+                        field.type == Float::class.javaObjectType -> field.set(this, json.getDouble(key))
+                        field.type == Double::class.javaObjectType -> field.set(this, json.getDouble(key))
+                        field.type == String::class.javaObjectType -> field.set(this, json.getString(key))
+
+                        field.type == Boolean::class.javaObjectType -> {
+
+                            if (json.get(key) is Boolean) {
+                                field.set(this, json.getBoolean(key))
+                            } else {
+                                field.set(this, json.getInt(key) == 1)
+                            }
+
+                        }
+
+
+                        JSONSerializable::class.java.isAssignableFrom(field.type) -> {
+                            val newInstance = field.type.newInstance() as JSONSerializable
+                            newInstance.deserialize(json.getJSONObject(key))
+                            field.set(this, newInstance)
+                        }
+
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+
+        }
+
+    }
+
+//    companion object {
+//
+//        fun isJsonSerializableClass(aClass: Class<*>) = when {
+//            aClass == Long::class.javaObjectType -> true
+//            aClass == Int::class.javaObjectType -> true
+//            aClass == Float::class.javaObjectType -> true
+//            aClass == Double::class.javaObjectType -> true
+//            aClass == String::class.javaObjectType -> true
+//            aClass == List::class.javaObjectType -> true
+//            JSONSerializable::class.java.isAssignableFrom(aClass) -> true
+//            else -> false
+//        }
+//    }
+
+}
