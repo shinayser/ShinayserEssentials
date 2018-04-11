91e660cfa01928728777acd5f62eb7126fdf0225
danieloliveira <danieloliveira@denox.com.br>
2018-04-11 11:57:22 -0300

    - Fixed lazy extras

--- library/src/main/java/com/shinayser/essentials/util/Fragment.kt
+++ library/src/main/java/com/shinayser/essentials/util/Fragment.kt
@@ -1,24 +1,22 @@
 package com.shinayser.essentials.util
 
-import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import com.shinayser.essentials.BuildConfig
 import org.jetbrains.anko.bundleOf
 
 /**
  * Created by Daniel Oliveira on 10/11/2017.
  */
 //Delegates
-fun Fragment.extraString(key: String) = lazy { if (arguments?.containsKey(key) == true) arguments?.getString(key) else null!! }
+fun Fragment.extraString(key: String) = lazy { arguments!!.getString(key)!! }
 
-fun Fragment.extraBoolean(key: String) = lazy { if (arguments?.containsKey(key) == true) arguments?.getBoolean(key) else null!! }
-fun Fragment.extraDouble(key: String) = lazy { if (arguments?.containsKey(key) == true) arguments?.getDouble(key) else null!! }
-fun Fragment.extraLong(key: String) = lazy { if (arguments?.containsKey(key) == true) arguments?.getLong(key) else null!! }
-fun Fragment.extraInt(key: String) = lazy { if (arguments?.containsKey(key) == true) arguments?.getInt(key) else null!! }
+fun Fragment.extraBoolean(key: String) = lazy { arguments!!.getBoolean(key)!! }
+fun Fragment.extraDouble(key: String) = lazy {arguments?.getDouble(key)!!}
+fun Fragment.extraLong(key: String) = lazy {arguments?.getLong(key)!!}
+fun Fragment.extraInt(key: String) = lazy {arguments?.getInt(key)!!}
 fun Fragment.extraBundle(key: String) = lazy { arguments?.getBundle(key)!! }
 fun <T : Parcelable> Fragment.extraParcelable(key: String) = lazy { arguments?.getParcelable<T>(key)!! }
 fun <T> Fragment.extraSerializable(key: String) = lazy { (arguments?.getSerializable(key) as T)!! }
